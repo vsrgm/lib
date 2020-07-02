e5123f83b05c6d3c0278bd1a58c5c3d95a4362be
Ananthapadmanaban <gapadmanaban.gap@gmail.com>
2020-07-02 16:38:04 +0530

    Temperature flicker controlled

--- Arduino/Temperature/Temperature.ino
+++ Arduino/Temperature/Temperature.ino
@@ -11,7 +11,7 @@ const int lm35_pin = A1;  /* LM35 O/P pin */
 #define SEG6 10
 #define SEG7 11
 #define SEG8 12
-
+#define AVERAGEDELAYCOUNT 600
 struct 
 {
   bool seg[8];
@@ -57,38 +57,69 @@ void setup()
   pinMode(SEG8, OUTPUT);
 }
 
+long readVcc()
+{
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+     ADMUX = _BV(MUX5) | _BV(MUX0) ;
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+ 
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+ 
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+ 
+  long result = (high<<8) | low;
+ 
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
+
 void loop()
 {
   unsigned char ch;
   int temp_adc_val;
   float temp_val;
-  int led_write;
+  static float temp_avg = 0;
+  static int led_write = 0;
   int a, b, c;
+  float refV;
+  static int count = AVERAGEDELAYCOUNT;  
+
+
+  refV = readVcc();
   temp_adc_val = analogRead(lm35_pin);  /* Read Temperature */
-  temp_val = (temp_adc_val * 4.88); /* Convert adc value to equivalent voltage */
-  led_write = temp_val;
-  temp_val = (temp_val / 10); /* LM35 gives output of 10mv/°C */
-#if 1
+  temp_val = (temp_adc_val * refV/1024); /* Convert adc value to equivalent voltage */
+  temp_val = (temp_val / 10); /* LM35 gives output of 10mv/°C */  
+  temp_avg = temp_avg + temp_val;
+  if (count == 0)
+  {
+    count = AVERAGEDELAYCOUNT;
+    led_write = temp_avg/AVERAGEDELAYCOUNT*10;
+    temp_avg = 0;
+  }
+
+  if (led_write == 0)
+  {
+    led_write = temp_val * 10;
+  }
+  
+  count--;
   Serial.print("Temperature = ");
   Serial.print(temp_val);
-  Serial.print(" Degree Celsius");
-  Serial.print(" led_write = ");  
-  Serial.print(led_write);
-  Serial.print(" 1st = ");
+  Serial.print(" Degree Celsius\r\n");
   a = led_write/100;
-  Serial.print(a);
-
-  Serial.print(" 2nd = ");
   b = (led_write - (a*100))/10;
-  Serial.print(b);
-  
-
-  Serial.print(" 3rd = ");
   c = (led_write - (a*100) - (b*10));
-  Serial.print(c);
-  Serial.print("\r\n");
-#endif
-  delay(15);
+
+  delay(5);
   a = led_write/100;
   digitalWrite(ENABLE1, HIGH);
   digitalWrite(ENABLE2, LOW);
@@ -99,7 +130,7 @@ void loop()
     digitalWrite(SEG1+i, val[a].seg[i]);
   }
 
-  delay(15);
+  delay(5);
   b = (led_write - (a*100))/10;
   
   digitalWrite(ENABLE1, LOW);
@@ -109,7 +140,7 @@ void loop()
   {
     digitalWrite(SEG1+i, val[11+b].seg[i]);
   }
-  delay(15);
+  delay(5);
   c = (led_write - (a*100) - (b*10));
   digitalWrite(ENABLE1, LOW);
   digitalWrite(ENABLE3, LOW);
