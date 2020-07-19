9db8d386ae12ce96d217136018679ff15c99367a
Ananthapadmanaban <gapadmanaban.gap@gmail.com>
2020-07-19 21:44:58 +0530

    Information about watering plants added

--- NodeMcu/Display/Display.ino
+++ NodeMcu/Display/Display.ino
@@ -7,106 +7,190 @@
 #include <LiquidCrystal_I2C.h>
 #include "DHT.h"
 #include "key.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+/************ Global State (you don't need to change this!) ******************/
 
-// Set the LCD address to 0x27 for a 16 chars and 2 line display
-LiquidCrystal_I2C lcd(0x3f, 16, 2);
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe plant = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME"/feeds/cmnplant"); // FeedName
+
+// Set the LCD address to 0x27/0x3f for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 DHT dht;
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Booting");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
+void setup()
+{
+	Serial.begin(115200);
+	Serial.println("Booting");
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+	while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+		Serial.println("Connection Failed! Rebooting...");
+		delay(5000);
+		ESP.restart();
+	}
 
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
+	// Port defaults to 8266
+	// ArduinoOTA.setPort(8266);
 
-  //Hostname defaults to esp8266-[ChipID]
-  ArduinoOTA.setHostname("DisplayModule");
+	//Hostname defaults to esp8266-[ChipID]
+	ArduinoOTA.setHostname("DisplayModule");
 
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
+	// No authentication by default
+	// ArduinoOTA.setPassword("admin");
 
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+	// Password can be set with it's md5 value as well
+	// MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+	// ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else { // U_FS
-      type = "filesystem";
-    }
+	ArduinoOTA.onStart([]() {
+			String type;
+			if (ArduinoOTA.getCommand() == U_FLASH) {
+			type = "sketch";
+			} else { // U_FS
+			type = "filesystem";
+			}
 
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
-  ArduinoOTA.begin();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+			// NOTE: if updating FS this would be the place to unmount FS using FS.end()
+			Serial.println("Start updating " + type);
+			});
+	ArduinoOTA.onEnd([]() {
+			Serial.println("\nEnd");
+			});
+	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+			Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+			});
+	ArduinoOTA.onError([](ota_error_t error) {
+			Serial.printf("Error[%u]: ", error);
+			if (error == OTA_AUTH_ERROR) {
+			Serial.println("Auth Failed");
+			} else if (error == OTA_BEGIN_ERROR) {
+			Serial.println("Begin Failed");
+			} else if (error == OTA_CONNECT_ERROR) {
+			Serial.println("Connect Failed");
+			} else if (error == OTA_RECEIVE_ERROR) {
+			Serial.println("Receive Failed");
+			} else if (error == OTA_END_ERROR) {
+			Serial.println("End Failed");
+			}
+			});
+	ArduinoOTA.begin();
+	Serial.println("Ready");
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
 
 
-  // initialize the LCD
-  lcd.begin();
+	// initialize the LCD
+	lcd.begin();
 
-  // Turn on the blacklight and print a message.
-  lcd.backlight();
-  lcd.setCursor(0, 0); // Cursor0 , Linea0
+	// Turn on the blacklight and print a message.
+	lcd.backlight();
 
-  lcd.print("Hello, world!");
-  lcd.setCursor(0, 1); // Cursor0 , Linea0
-  lcd.print("Hello!");
-
-  dht.setup(D3);   /* D1 is used for data communication */
-  lcd.clear();
+	dht.setup(D3);   /* D1 is used for data communication */
+	lcd.clear();
+  mqtt.subscribe(&plant);
 
 }
 
 void loop() {
-  ArduinoOTA.handle();
-  delay(dht.getMinimumSamplingPeriod()); /* Delay of amount equal to sampling period */
-  int humidity = dht.getHumidity();/* Get humidity value */
-  int temperature = dht.getTemperature();/* Get temperature value */
-  if (!strncmp(dht.getStatusString(), "OK", 2))
-  { 
-    lcd.setCursor(0, 0); // Cursor0 , Linea0
-    lcd.print("Humidity : ");
-    lcd.print(humidity);
-    lcd.print("%");
+	enum feeds
+	{
+		BPLANTS,
+		TPLANTS,
+		RESETREPORT,
+		REPORT,
+	};
+	int ret, type, value1, value2, value3, value4, levelb, levelt;
+	Adafruit_MQTT_Subscribe *subscription;
 
-    lcd.setCursor(0, 1); // Cursor0 , Linea0
-    lcd.print("Temp     : ");
-    lcd.print(temperature);
-    lcd.print("C");
-  }
+	ArduinoOTA.handle();
+	ret = MQTT_connect();
+
+	delay(dht.getMinimumSamplingPeriod()); /* Delay of amount equal to sampling period */
+	int humidity = dht.getHumidity();/* Get humidity value */
+	int temperature = dht.getTemperature();/* Get temperature value */
+	if (!strncmp(dht.getStatusString(), "OK", 2))
+	{ 
+		lcd.setCursor(0, 0); // Cursor0 , Linea0
+		lcd.print("H:");
+		lcd.print(humidity);
+		lcd.print("% ");
+
+		//lcd.setCursor(0, 1); // Cursor0 , Linea0
+		lcd.print("T:");
+		lcd.print(temperature);
+		lcd.print("C");  
+	}
+ 
+	while ((subscription = mqtt.readSubscription(5000)))
+	{
+		if (subscription == &plant)
+		{
+			sscanf((char *)plant.lastread, "%d,%d,%d,%d,%d,%d,%d",
+					&type, &value1, &value2, &value3, &value4,
+					&levelb, &levelt);
+			if (type == REPORT)
+			{
+				lcd.setCursor(12, 0);		
+				lcd.print("B");
+				lcd.print(levelb);
+				lcd.print("T");
+				lcd.print(levelt);
+
+				lcd.setCursor(0, 1);
+				lcd.print("B");		
+				lcd.print(value1);
+				lcd.print("/");
+				lcd.print(value2);
+				lcd.print(" ");
+				lcd.print("T");
+				lcd.print(value3);
+				lcd.print("/");
+				lcd.print(value4);
+			}
+		}
+	}
+}
+
+unsigned char MQTT_connect()
+{
+	int8_t ret;
+
+	// Stop if already connected.
+	if (mqtt.connected())
+	{
+		return 1;
+	}
+
+	Serial.print("Connecting to MQTT... ");
+
+	uint8_t retries = 3;
+
+	while ((ret = mqtt.connect()) != 0)
+	{ // connect will return 0 for connected
+		Serial.println(mqtt.connectErrorString(ret));
+		Serial.println("Retrying MQTT connection in 5 seconds...");
+		mqtt.disconnect();
+		delay(5000);  // wait 5 seconds
+		retries--;
+		if (retries == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
