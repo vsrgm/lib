7a9b7fff8702cdf943f03f23b5ddbb472196f1b6
Ananthapadmanaban <gapadmanaban.gap@gmail.com>
2020-07-19 16:24:01 +0530

    OTA Firmware update support added

--- NodeMcu/ifttt_waterpump/ifttt_waterpump.ino
+++ NodeMcu/ifttt_waterpump/ifttt_waterpump.ino
@@ -1,5 +1,9 @@
 //TODO : Implement WatchDog to safe reboot in case if any hang in the board
-
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <Wire.h> 
 #include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
@@ -51,6 +55,8 @@ enum feeds
 	RESETREPORT,
 	REPORT,
 };
+const char* ssid = WLAN_SSID;
+const char* password = WLAN_PASS;
 
 void setup()
 {
@@ -63,8 +69,69 @@ void setup()
 	digitalWrite(RELAY3, ENABLE_HIGH);
 	pinMode(RELAY4, OUTPUT);
 	digitalWrite(RELAY4, ENABLE_HIGH);
-	pinMode(WATERLEVELTERRACE, INPUT);
-	pinMode(WATERLEVELBALCONY, INPUT);
+	pinMode(WATERLEVELTERRACE, INPUT_PULLUP);
+	pinMode(WATERLEVELBALCONY, INPUT_PULLUP);
+
+
+	Serial.begin(115200);
+	Serial.println("Booting");
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+	while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+		Serial.println("Connection Failed! Rebooting...");
+		delay(5000);
+		ESP.restart();
+	}
+
+	// Port defaults to 8266
+	// ArduinoOTA.setPort(8266);
+
+	//Hostname defaults to esp8266-[ChipID]
+	ArduinoOTA.setHostname("Waterpump");
+
+	// No authentication by default
+	// ArduinoOTA.setPassword("admin");
+
+	// Password can be set with it's md5 value as well
+	// MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+	// ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+	ArduinoOTA.onStart([]() {
+			String type;
+			if (ArduinoOTA.getCommand() == U_FLASH) {
+			type = "sketch";
+			} else { // U_FS
+			type = "filesystem";
+			}
+
+			// NOTE: if updating FS this would be the place to unmount FS using FS.end()
+			Serial.println("Start updating " + type);
+			});
+	ArduinoOTA.onEnd([]() {
+			Serial.println("\nEnd");
+			});
+	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+			Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+			});
+	ArduinoOTA.onError([](ota_error_t error) {
+			Serial.printf("Error[%u]: ", error);
+			if (error == OTA_AUTH_ERROR) {
+			Serial.println("Auth Failed");
+			} else if (error == OTA_BEGIN_ERROR) {
+			Serial.println("Begin Failed");
+			} else if (error == OTA_CONNECT_ERROR) {
+			Serial.println("Connect Failed");
+			} else if (error == OTA_RECEIVE_ERROR) {
+			Serial.println("Receive Failed");
+			} else if (error == OTA_END_ERROR) {
+			Serial.println("End Failed");
+			}
+			});
+	ArduinoOTA.begin();
+	Serial.println("Ready");
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
+
 
 	// initialize EEPROM with predefined size
 	EEPROM.begin(EEPROM_SIZE);
@@ -72,14 +139,13 @@ void setup()
 	if (Magic == 0xAB)
 	{
 		bcnt = EEPROM.read(BCNT);
- 		tcnt = EEPROM.read(TCNT);
- 		btotal = EEPROM.read(BTOTAL);
- 		ttotal = EEPROM.read(TTOTAL);
+		tcnt = EEPROM.read(TCNT);
+		btotal = EEPROM.read(BTOTAL);
+		ttotal = EEPROM.read(TTOTAL);
 	}else
 	{
 		resetEEROM(0,0,0,0);
 	}
-	Serial.begin(115200);
 
 	// Connect to WiFi access point.
 	Serial.println(); Serial.println();
@@ -116,11 +182,14 @@ void resetEEROM(int bcnt, int tcnt, int btotal, int ttotal)
 void loop()
 {
 	int count = 0;
+	ArduinoOTA.handle();
 	MQTT_connect();
 	int balwaterlevellow = 0, terracewaterlevellow = 0;
 	char publish[100];
 	int type, value1, value2, value3, value4;
- 
+
+	balwaterlevellow = digitalRead(WATERLEVELBALCONY);
+	terracewaterlevellow = digitalRead(WATERLEVELTERRACE);
 	Adafruit_MQTT_Subscribe *subscription;
 	while ((subscription = mqtt.readSubscription(5000)))
 	{
@@ -129,8 +198,7 @@ void loop()
 			Serial.print(F("GotB: "));
 			Serial.println((char *)plant.lastread);
 			sscanf((char *)plant.lastread, "%d,%d,%d,%d,%d", &type, &value1, &value2, &value3, &value4);
-			balwaterlevellow = digitalRead(WATERLEVELBALCONY);
-			terracewaterlevellow = digitalRead(WATERLEVELTERRACE);
+
 
 			if (type == BPLANTS)
 			{
@@ -165,7 +233,7 @@ void loop()
 
 			}else if(type == TPLANTS)
 			{
-				if (value1)// && !terracewaterlevellow)
+				if (value1 && terracewaterlevellow)
 				{
 					digitalWrite(RELAY3, !ENABLE_HIGH);
 					digitalWrite(RELAY4, !ENABLE_HIGH);
